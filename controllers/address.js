b8cc486c6cb6cf759851de6eae0febbd82fe0d74
Antoine FAGNERE <138126838+Nefaaa1@users.noreply.github.com>
2024-03-07 16:53:32 +0100

    add detail of adress page

---
+++ controllers/address.js
@@ -0,0 +1,24 @@
+import fs from 'fs'
+import {parse} from 'csv-parse';
+const port = 3000;
+const baseUrl = `http://localhost:${port}`
+
+export default (req, res) => {
+    const idAddress = req.params.id;
+    fs.readFile('./public/data/address.csv', (err, content) =>{
+        if (err) {
+            res.writeHead(500, { "Content-type": "text/plain" })
+            res.end('Internal Servor error: ', err)
+        } else {
+            parse(content, (error, result)=>{
+                if (error) {
+                    res.writeHead(500, { "Content-type": "text/plain" })
+                    res.end('Internal Servor error: ', error)
+                } else {
+                    const addressOfID = result.find(result => result[0] === idAddress);
+                    res.render('address.ejs', {baseUrl: baseUrl, address : addressOfID})
+                }
+            })
+        }
+    })
+}
