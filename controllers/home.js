f8aa0d83d6dd891b58aa307eae15fbbf5db939a7
Antoine FAGNERE <138126838+Nefaaa1@users.noreply.github.com>
2024-03-07 16:02:44 +0100

    add List of adress in home page v2

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,23 @@
+import fs from 'fs'
+import {parse} from 'csv-parse';
 const port = 3000;
 const baseUrl = `http://localhost:${port}`
 
 export default (req, res) => {
-    res.render('index.ejs', {baseUrl: baseUrl})
+    fs.readFile('./public/data/address.csv', (err, content) =>{
+        if (err) {
+            res.writeHead(500, { "Content-type": "text/plain" })
+            res.end('Internal Servor error: ', err)
+        } else {
+            parse(content, (error, result)=>{
+                if (err) {
+                    res.writeHead(500, { "Content-type": "text/plain" })
+                    res.end('Internal Servor error: ', error)
+                } else {
+                    console.log(result)
+                    res.render('index.ejs', {baseUrl: baseUrl, address : result})
+                }
+            })
+        }
+    })
 }
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
+import router from "./router/address.js";
 
 const app = express();
 const port = 3000;
+const baseUrl = `http://localhost:${port}`;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/', router)
 
 
 app.listen(port, () => {
-    console.log("Serveur sur le port : ", port);
+    console.log("Serveur lancé sur : ", baseUrl);
 })
